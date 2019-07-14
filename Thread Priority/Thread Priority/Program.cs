7d582a7c6fe9a219439b8b9f3c380729fb2924f3
niharika23061993 <harika.net1212@gmail.com>
2019-07-14 06:29:08 -0400

    Thread Priority

---
+++ Thread Priority/Thread Priority/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Priority
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********Current Thread Informations**************n");
+            Thread t = Thread.CurrentThread;
+            t.Name = "Primary_Thread";
+
+            Console.WriteLine("Thread Name: {0}", t.Name);
+            Console.WriteLine("Thread Status: {0}", t.IsAlive);
+            Console.WriteLine("Priority: {0}", t.Priority);
+
+            Console.ReadKey();
+        }
+    }
+}
